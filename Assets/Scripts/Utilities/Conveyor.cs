f25d9052da917da1d74930af6f2b023d81cd86d6
KyaroMeow <164043414+KyaroMeow@users.noreply.github.com>
2025-10-05 19:34:01 +0300

    new conveyor

--- Assets/Scripts/Utilities/Conveyor.cs
+++ Assets/Scripts/Utilities/Conveyor.cs
@@ -6,6 +6,7 @@ public class Conveyor : MonoBehaviour
 {
     [Header("Conveyor Settings")]
     public float conveyorSpeed = -2f;
+    public float conveyorVisualSpeed = 1f;
     public bool canMove = true;
     Rigidbody rb;
     void Start()
@@ -25,5 +26,6 @@ public class Conveyor : MonoBehaviour
         Vector3 pos = rb.position;
         rb.position += Vector3.back * conveyorSpeed * Time.fixedDeltaTime;
         rb.MovePosition(pos);
+        GetComponent<Renderer>().material.mainTextureOffset += new Vector2(conveyorVisualSpeed * Time.fixedDeltaTime, 0);
     }
 }
